17296f5d6db2f4acf1e42c4606a76dba43857179
Arisveus <arielmatteibisatto12@gmail.com>
2025-03-13 11:26:36 -0300

    Aula 2- Configuração do Twig

---
+++ twig_carregar.php
@@ -0,0 +1,9 @@
+<?php
+// Carrega o carregador do Composer
+require_once('vendor/autoload.php');
+
+// Loader é quem carrega os arquivos HTML
+$loader = new \Twig\Loader\FilesystemLoader('./templates');
+
+// Criar o objeto Twig
+$twig = new \Twig\Environment($loader);
